10a172df64a0f76a17ef876a3e3d8bb153ac8093
anvyzhang <anvyzhang@gmail.com>
2014-05-14 10:36:35 +0800

    0.0.3

--- lib/progbar.rb
+++ lib/progbar.rb
@@ -1,12 +1,13 @@
 class Array
   def prog_each(&block) 
+    bar_length = (`tput cols` || 80).to_i - 30
     time_now = Time.now
     total = self.count
     last_flush = 0
     flush_time = 1
     self.each_with_index{|element, x|
       cur = (x+1) * 100 / total
-      time_left = (((Time.now - time_now) * (100 - cur)) / cur).to_i
+      time_left = (((Time.now - time_now) * (100 - cur)).to_f / cur).ceil
       if (Time.now - last_flush).to_i >= flush_time or time_left < 1
         time_left_graceful = Time.at(time_left).utc.strftime("%H:%M:%S")
         if time_left > 86400
@@ -15,7 +16,8 @@ class Array
           time_left_graceful = time_left_graceful.join(":")
         end
         print "\r"
-        print "[" << (["#"] * cur).join << (["-"] * (100-cur)).join << "] #{cur}% [#{time_left_graceful} left]"
+        cur_len = (bar_length * (x + 1)) / total
+        print "[" << (["#"] * cur_len).join << (["-"] * (bar_length - cur_len)).join << "] #{cur}% [#{time_left_graceful} left]"
         last_flush = Time.now
       end
       block.call element if block
--- progbar.gemspec
+++ progbar.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'progbar'
-  s.version     = '0.0.2'
-  s.date        = '2014-05-13'
+  s.version     = '0.0.3'
+  s.date        = '2014-05-14'
   s.summary     = "Print progress bar for array iteration"
   s.description = "Print progress bar for array iteration"
   s.authors     = ["anvyzhang"]
